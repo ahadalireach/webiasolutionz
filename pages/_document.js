2a75ea82c787ad74736ece85bb3db43352b29c65
bfmahad <ahadali.reach@gmail.com>
2024-04-09 20:30:26 +0500

    First Commit

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="author" content="Ahad Ali" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="keywords" content="Webia Solutionz" />
+        <meta name="description" content="Webia Solutionz" />
+        <title>Webia Solutionz</title>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+        <link href="assets/css/style.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
